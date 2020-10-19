3ed9821de28e04382af9fda397aba5bfbb12b5de
Anna <45462671+anna2990@users.noreply.github.com>
2020-10-19 17:14:03 +0300

    implement vuex

---
+++ src/components/Todos.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="todos">
+    <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Todos",
+  methods: { ...mapActions(["fetchTodos"]) },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  }
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 3rem;
+  margin-top: 2rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+</style>
